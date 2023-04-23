97f0ffd5f84556182623346d8578547660d5ce7f
unknown <merabtodua7@gmail.com>
2023-04-23 19:37:49 +0400

    first commit

---
+++ data.js
@@ -0,0 +1,14 @@
+
+ //Define an object with links to external sites
+  export const links = {
+  "git1": "https://github.com/merab7/witwit.git",
+  "net1": "https://rococo-beijinho-26e2e1.netlify.app/",
+  "git2": "https://github.com/merab7/Narutofy.git",
+  "net2": "https://adorable-tapioca-3687e3.netlify.app/",
+  "git3": "https://github.com/merab7/Mdiner.git",
+  "net3": "https://thunderous-salmiakki-7360b4.netlify.app/",
+  "git4": "https://github.com/merab7/Irobot.git",
+  "net4": "https://sparkly-melba-27e36b.netlify.app/",
+  "git5": "https://github.com/merab7/meme-picker.git",
+  "net5": "https://resplendent-scone-066b40.netlify.app/"
+};
---
+++ index.js
@@ -0,0 +1,149 @@
+import { links } from "/data.js";
+// Get the menu bar element and the dropdown menu element
+const menuBar = document.getElementById('menubar');
+const dropdownMenu = document.getElementById('dropdownmenu');
+
+// Get all the n1-post elements and linkbtn elements
+const posts = document.querySelectorAll('.n1-post');
+const linkbtns= document.querySelectorAll('.linkbtn');
+const buttons= document.querySelectorAll('.golink');
+const viewmore=document.getElementById('viewmore');
+const homebtns=document.querySelectorAll('.home');
+const projectsnavbar=document.querySelectorAll('.projectsinnavbar');  
+const logos=document.querySelectorAll('.logotext')
+const abouts=document.querySelectorAll('.about')
+const fcb=document.getElementById('fcbtn')
+const insta=document.getElementById('instbtn')
+const gmail=document.getElementById('gmailbtn')
+const download=document.getElementById('download')
+
+// Initialize a flag to keep track of whether the menu has been clicked or not
+let isClicked = false;
+
+// Add a click event listener to the menu bar element
+menuBar.addEventListener('click', function() {
+  // If the menu has not been clicked yet, show the dropdown menu
+  if (!isClicked) {
+    dropdownMenu.style.display = 'flex';
+  } 
+  // If the menu has been clicked already, hide the dropdown menu
+  else {
+    dropdownMenu.style.display = 'none';
+  }
+  // Invert the value of isClicked to toggle the menu state
+  isClicked = !isClicked;
+});
+
+// Add a click event listener to the document
+document.addEventListener('click', function(e) {
+  const target = e.target;
+  // If the target of the click event is not the menu bar or the dropdown menu, hide the dropdown menu
+  if (target !== menuBar && target !== dropdownMenu) {
+    dropdownMenu.style.display = 'none';
+    isClicked = false;
+  }
+});
+
+// Hide all link buttons initially
+linkbtns.forEach(function(linkbtn) {
+  linkbtn.style.display = 'none';
+});
+
+// For each post, add a click event listener
+posts.forEach(function(post) {
+  post.addEventListener('click', function() {
+    // Find the link button inside the clicked post
+    const linkbtn = post.querySelector('.linkbtn');
+    // Toggle the link button's display between 'none' and 'flex'
+    linkbtn.style.display = (linkbtn.style.display === 'none') ? 'flex' : 'none';
+  });
+});
+
+// For each button, add a click event listener
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Get the id of the button
+    const id = button.getAttribute('id');
+    // Get the link associated with the id from the links object
+    const link = links[id];
+    // Redirect the user to the link
+    window.location.href = link;
+  });
+});
+
+
+
+
+logos.forEach(function(logo){
+
+  logo.addEventListener('click', function(){
+  
+    window.location.href = "index.html";
+  
+  })
+  
+  })
+
+// Add a click event listener to each home button
+homebtns.forEach(function(homebtn) {
+  homebtn.addEventListener('click', function() {
+    // Redirect the user to the home page
+    window.location.href = "index.html";
+  });
+});
+
+
+  projectsnavbar.forEach(function(projectbtn){
+
+    projectbtn.addEventListener('click', function(){
+    
+      window.location.href = "project.html";
+    
+    });
+    
+    });
+
+    abouts.forEach(function(about){
+
+      about.addEventListener('click',function(){
+    
+        window.location.href = "aboutme.html";
+    
+      })
+    
+    
+    })
+    
+
+viewmore.addEventListener('click', function(){
+  window.location.href = "project.html";
+})
+
+
+
+
+
+fcb.addEventListener('click',function(){
+
+  window.location.href = "https://www.facebook.com/merabi.todua/";
+
+})
+
+insta.addEventListener('click',function(){
+
+  window.location.href = "https://www.instagram.com/merab_todua0/";
+
+})
+
+
+gmail.addEventListener('click', function(){
+
+  window.location.href = "https://mail.google.com/mail/?authuser=2&ogbl";
+
+})
+
+download.addEventListener('click',function(){
+
+  window.location.href='https://drive.google.com/file/d/1Jkhpz81GmSRGNpqWaTwWPLTIm1O4cp3e/view?usp=sharing'
+
+})
